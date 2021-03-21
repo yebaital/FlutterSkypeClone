28dae68845e266d872f72165f558a14a6c2f4e8e
Yousef <yousef.baitalmal@gmail.com>
2021-03-21 16:30:14 +0300

    Added bottom nav

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,85 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:skype_clone/utils/universal_variables.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  PageController pageController;
+  int _page = 0;
+  int labelFontSize = 10;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  void navigationTapped(int page){
+    pageController.jumpToPage(page);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+      backgroundColor: UniversalVariables.blackColor,
+      body: PageView(
+        children: [
+          Center(child: Text("Chat List Screen", style: TextStyle(color: Colors.white),)),
+          Center(child: Text("Call Logs", style: TextStyle(color: Colors.white),)),
+          Center(child: Text("Contacts Screen", style: TextStyle(color: Colors.white),)),
+        ],
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: Container(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: CupertinoTabBar(
+            backgroundColor: UniversalVariables.blackColor,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.chat,
+                  color: (_page == 0)
+                      ? UniversalVariables.lightBlueColor
+                      : UniversalVariables.greyColor,
+                ),
+                label: "Chats",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.call,
+                  color: (_page == 1)
+                      ? UniversalVariables.lightBlueColor
+                      : UniversalVariables.greyColor,
+                ),
+                label: "Calls",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.contact_phone,
+                  color: (_page == 2)
+                      ? UniversalVariables.lightBlueColor
+                      : UniversalVariables.greyColor,
+                ),
+                label: "Contacts",
+              ),
+            ],
+            onTap: navigationTapped,
+            currentIndex: _page,
+          ),
+        ),
       ),
     );
   }
