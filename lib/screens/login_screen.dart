36c28e0141c8e9ae896d65567c0ebd0cd07724be
Yousef <yousef.baitalmal@gmail.com>
2021-03-21 16:09:46 +0300

    Added sign in with google via Firebase Auth.

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,89 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:skype_clone/resources/firebase_repository.dart';
+import 'package:skype_clone/screens/home_screen.dart';
+import 'package:skype_clone/utils/universal_variables.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  FirebaseRepository _repository = FirebaseRepository();
+  bool isLoginPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: loginButton(),
+          ),
+          isLoginPressed
+              ? Center(
+            child: CircularProgressIndicator(),
+          )
+              : Container()
+        ],
+      ),
+      backgroundColor: UniversalVariables.blackColor,
+    );
+  }
+
+  Widget loginButton() {
+    return Shimmer.fromColors(
+      baseColor: Colors.white,
+      highlightColor: UniversalVariables.senderColor,
+      child: TextButton(
+        onPressed: () => performLogin(),
+        child: Padding(
+          padding: EdgeInsets.all(35),
+          child: Text(
+            "LOGIN",
+            style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.w900, letterSpacing: 1.2),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void performLogin() {
+    setState(() {
+      isLoginPressed = true;
+    });
+    _repository.signIn().then((FirebaseUser user) {
+      if (user != null) {
+        authenticateUser(user);
+      } else {
+        print("There was an error");
+      }
+    });
+  }
+
+  void authenticateUser(FirebaseUser user) {
+    _repository.authenticateUser(user).then((isNewUser) {
+      setState(() {
+        isLoginPressed = false;
+      });
+
+      if (isNewUser) {
+        _repository.addDataToDb(user).then((value) {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) {
+                return HomeScreen();
+              }));
+        });
+      }
+      else {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) {
+              return HomeScreen();
+            }));
+      }
+    });
+  }
+}
